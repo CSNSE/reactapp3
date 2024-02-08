09c6e103a3b94dcf572bc046b1d1af572d5cbc5c
LandonB5 <landon.b5@icloud.com>
2024-02-07 20:27:28 -0500

    https://github.com/CSNSE/reactapp3/issues/11

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import './App.css';
 import { withAuthenticator } from '@aws-amplify/ui-react';
-import {WelcomePage, Header, FoodCardCollection, Info, ItemInfo, EditItem, NoteCreateForm, ListCardCollection} from "./ui-components";
+import {WelcomePage, Header,ListHeader, NewList, EditList, FoodCardCollection, Info, ItemInfo, EditItem, NoteCreateForm, ListCardCollection} from "./ui-components";
 import EditNote from './EditNote';
 import {Routes, Route} from 'react-router-dom'
 
@@ -19,12 +19,14 @@ class App extends Component {
 
     <div className="App"><header className="App-header">
 <Routes>
-<Route exact path='/' element={<div><Header/><ListCardCollection/></div>} />
+<Route exact path='/' element={<div><ListHeader/><ListCardCollection/></div>} />
 <Route exact path='/1edit/:cid' element={<div><Header/><FoodCardCollection/></div>} />
 <Route exact path='/0' element={<div><WelcomePage/></div>} />
 <Route exact path='/new' element={<div><Header/><ItemInfo/></div>} />
 <Route exact path='/info' element={<div><Info/></div>} />
 <Route exact path='/edit/:cid' element={<EditNote/>} />
+<Route exact path='/ediList/:cid' element={<EditList/>} />
+<Route exact path='/newList' element={<div><Header/><NewList/></div>} />
 {/* <Route exact path='/1edit/:cid' element={<NoteUpdateForm/>} /> */}
 <Route exact path='/1new' element={<div><Header/><NoteCreateForm/></div>} />
 {/* {<Route exact path='/new' element= {<UICreateNote/>} /> } */}
